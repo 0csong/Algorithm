40b7c133bd593c91860ec879876cd09d51e1d4d5
0csong <79524109+0csong@users.noreply.github.com>
2021-04-20 23:01:13 +0900

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,17 @@
+#BFS는 큐사용
+from collections import deque
+
+def bfs(graph,start,visited):
+    queue=deque([start])
+    visited[start]=True
+    while queue: #큐가빌때까지 반복
+        v=queue.popleft()
+        print(v,end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
+graph=[[],[2,3,8],[1,7],[1,4,5],[3,5],
+      [3,4],[7],[2,6,8],[1,7]] 
+visited=[False]*9 #모든 노드 방문 x로
+bfs(graph,1,visited)
