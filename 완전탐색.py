dc4218da243c54c83a9c9888a0a8450d0b233daf
0csong <79524109+0csong@users.noreply.github.com>
2021-04-23 23:34:26 +0900

    Add files via upload

---
+++ 완전탐색.py
@@ -0,0 +1,12 @@
+h=int(input())
+
+count=0
+for i in range(h+1): #h시까지
+    for j in range(60): #j분
+        for k in range(60):#k초
+            if '3' in str(i)+str(j)+str(k):
+                count+=1
+
+print(count)
+
+#00시 00분 00초부터 h시 59분 59초까지 3이 포함되는 시간 개수
