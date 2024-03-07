a8e0a983fe073727b6b3568d8dc9ba275d010cc8
송영도 <79524109+0csong@users.noreply.github.com>
2024-03-07 22:20:33 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 50936 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+from collections import deque
+n=int(input())
+
+q=deque()
+for i in range(1,n+1):
+    q.append(i)
+
+for i in range(n-1):
+    q.popleft()
+    q.append(q[0])
+    q.popleft()
+print(q[0])
+
