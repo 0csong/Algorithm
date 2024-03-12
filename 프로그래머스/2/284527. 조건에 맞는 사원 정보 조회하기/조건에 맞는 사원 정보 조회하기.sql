98ba038b8a23ccce527bf6e78c79b781f24e3b70
송영도 <79524109+0csong@users.noreply.github.com>
2024-03-12 15:56:30 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -6,8 +6,3 @@ JOIN HR_DEPARTMENT B ON A.DEPT_ID=B.DEPT_ID
 JOIN HR_GRADE C ON A.EMP_NO=C.EMP_NO
 GROUP BY C.EMP_NO
 ORDER BY SCORE DESC LIMIT 1
-# select e.emp_no, e.emp_name, e.position, e.email 
-# from hr_employees e
-# join hr_grade g using(emp_no)
-# join hr_department d using(dept_id)
-# group by e.emp_no
