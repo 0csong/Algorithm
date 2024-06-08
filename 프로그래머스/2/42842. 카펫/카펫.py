60dab7da7ea8b54519eca665874b094ffb0f13d1
송영도 <79524109+0csong@users.noreply.github.com>
2024-06-08 22:16:58 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,11 @@
 def solution(brown, yellow):
     answer = []
-    total = brown + yellow                  # a * b = total
-    for b in range(1,total+1):
-        if total % b == 0:            # total / b = a
-            a = total / b
-            if a >= b:                      # a >= b
-                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
-                    return [a,b]
-            
+    total=brown+yellow
+    for i in range(1,total+1):
+        if total%i==0:
+            a=total//i
+            b=total//a
+            if (a-2) * (b-2)==yellow:
+                return [a,b]
+    
     return answer
